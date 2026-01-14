f0c3edc5f5b63424943380d2f2269df274c4b743
Bilal Nazeer <bilaldata50@gmail.com>
2026-01-14 22:55:39 +0500

    Commit Message

---
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.github
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Find Views
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+
+        // Handle button click
+        btnSignIn.setOnClickListener {
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter username or password", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Welcome $username!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
